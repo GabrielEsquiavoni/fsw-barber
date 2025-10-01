90a1c99a743b2ad825529de15bbb0cafee514bdc
GabrielEsquiavoni <gabrielazevedo.2004@gmail.com>
2025-10-01 18:01:01 -0300

    feat: ensure only signed in user can book

---
+++ app/_components/sign-in-dialog.tsx
@@ -0,0 +1,33 @@
+import { Button } from "./ui/button"
+import { signIn } from "next-auth/react"
+import { DialogDescription, DialogHeader, DialogTitle } from "./ui/dialog"
+import Image from "next/image"
+
+const SignInDialog = () => {
+  const handleLoginWithGoogleClick = () => signIn("google")
+  return (
+    <>
+      <DialogHeader>
+        <DialogTitle>Faça seu Login</DialogTitle>
+        <DialogDescription>
+          Conecte-se usando sua conta do Google
+        </DialogDescription>
+      </DialogHeader>
+      <Button
+        variant="outline"
+        className="gap-1 font-bold"
+        onClick={handleLoginWithGoogleClick}
+      >
+        <Image
+          alt="fazer login com o google"
+          src="/google.svg"
+          width={18}
+          height={18}
+        />
+        Google
+      </Button>
+    </>
+  )
+}
+
+export default SignInDialog
